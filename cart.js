12c7661e770c4748b9b5218b68eb91bd0ef2e37c
Roulianosss <julien.ortscheid@gmail.com>
2022-11-08 15:28:22 +0100

    commit

---
+++ cart.js
@@ -0,0 +1,49 @@
+const cartCard = document.querySelector('#cart-card')
+
+function displayCart() {
+    fetch('http://localhost:3000/cart')
+    .then(res => res.json())
+    .then(allCartItem => {
+        if (!allCartItem) return
+        cartCard.innerHTML = `
+            <h2>My Cart</h2>
+        ` 
+        let itemsList = document.createElement('div')
+        itemsList.className = 'items-list'
+        allCartItem.forEach(item => {
+            console.log(item)
+            itemsList.innerHTML += `
+                <div class='item-row' data-id='${item._id}'>
+                    <p>${item.departure}>${item.arrival}</p>
+                    <p>${item.date}</p>
+                    <p>${item.price}</p>
+                    <span class="delete-btn">✖</span>
+                </div> 
+            `
+
+        })
+        cartCard.append(itemsList)
+        document.querySelectorAll('.delete-btn').forEach(btn => btn.addEventListener('click', handleRemove))
+})
+}
+displayCart()
+
+function handleRemove(e) {
+    fetch(`http://localhost:3000/cart/delete/${this.parentNode.getAttribute('data-id')}`, {
+		method: 'DELETE',
+		headers: { 'Content-Type': 'application/json' },
+	})
+        .then(res => res.json())
+        .then(data => displayCart())
+
+
+}
+
+    // {
+    //     "_id": "636a5deb5537701feb45e2ab",
+    //     "departure": "Paris",
+    //     "arrival": "Marseille",
+    //     "date": "2022-11-08T11:00:15.780Z",
+    //     "price": 83,
+    //     "__v": 0
+    // }
